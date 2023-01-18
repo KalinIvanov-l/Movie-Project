d2ba36b45cb799247e0bd7c22a623aeb3bbc7624
Kalin Ivanov <kalin2578@gmail.com>
2023-01-18 12:18:23 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Customers from "./components/common/customers";
+import Rental from "./components/common/rental";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rental" component={Rental}></Route>
+        <Redirect from="/" to="movies" />
+      </Switch>
     </main>
   );
 }
