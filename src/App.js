f3e0c72824d817168d61e96ccd8cbf6ce28a4f97
Kalin Ivanov <kalin2578@gmail.com>
2023-01-18 12:43:52 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import Customers from "./components/common/customers";
 import Rental from "./components/common/rental";
 import NotFound from "./components/common/notFound";
+import NavBar from "./components/common/navBar";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rental" component={Rental}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rental" component={Rental}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
