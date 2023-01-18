fbdbe7bc91eadf763f762583a1e159b67cac7143
Kalin Ivanov <kalin2578@gmail.com>
2023-01-18 12:24:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Movies from "./components/movies";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Customers from "./components/common/customers";
 import Rental from "./components/common/rental";
+import NotFound from "./components/common/notFound";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
         <Route path="/movies" component={Movies}></Route>
         <Route path="/customers" component={Customers}></Route>
         <Route path="/rental" component={Rental}></Route>
-        <Redirect from="/" to="movies" />
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" exact to="movies" />
+        <Redirect to="/not-found" />
       </Switch>
     </main>
   );
